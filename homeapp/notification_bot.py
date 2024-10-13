4c45bd8359fafe7b6f9d0b12826dd3f59ce7c845
hlorka2c <arbuzzzzz44@mail.ru>
2024-10-14 02:30:56 +0300

    вернул бота

--- homeapp/notification_bot.py
+++ homeapp/notification_bot.py
@@ -1,16 +1,15 @@
-# from aiogram import Bot
-# from aiogram.dispatcher import Dispatcher
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
 
-# from advo_site.settings import TG_KEY, CHAT_ID, ADMIN_ID
+from advo_site.settings import TG_KEY, CHAT_ID, ADMIN_ID
 
 
-# bot = Bot(token=TG_KEY)
-# dp = Dispatcher(bot)
+bot = Bot(token=TG_KEY)
+dp = Dispatcher(bot)
 
 
 async def send_telegram_message(message):
-    # try:
-    #     await bot.send_message(chat_id=CHAT_ID, text=message)
-    # except Exception as e:
-    #     await bot.send_message(chat_id=ADMIN_ID, text=e)
-    pass
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception as e:
+        await bot.send_message(chat_id=ADMIN_ID, text=e)
